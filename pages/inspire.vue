5d73b3f426ea11bb8831adfb038e291e0513720c
Peakgod <2832312250@qq.com>
2019-01-02 18:04:31 +0800

    页面修改

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,18 +1,26 @@
 <template>
-  <v-layout class="container">
-    <div class="container-content">
-      <div class="content-right-btn">
-        <v-btn color="#fff" flat nuxt to="/">返回主页面</v-btn>
-      </div>
-      
-      <div class="content-right-top">
-        <div class="content-right-top-left">
-           <div class="content-address" slot="activator" color="primary" dark>
-             <img src="~/static/inco-location.png"/>
-             <span>{{this.location}}dasdadd</span>
-               <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-               <v-btn slot="activator" color="primary" dark>选择地址</v-btn>
-               <v-card>
+  
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap justify-center>
+      <v-flex xs12>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">天气详情</v-card-text>
+          <div class="content-right-btn">
+            <v-btn color="#fff" flat nuxt to="/">返回主页面</v-btn>
+          </div>
+        </v-card>
+      </v-flex>
+      <v-flex xs6>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">
+            
+            <div class="content-right-top-left">
+              <div class="content-address" slot="activator" color="primary" dark>
+                <img src="~/static/inco-location.png"/>
+                <span>{{this.location}}dasdadd</span>
+                <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+                <v-btn slot="activator" color="primary" dark>选择地址</v-btn>
+                <v-card>
                  <v-toolbar dark color="primary">
                    <v-btn icon dark @click="dialog = false">
                      <v-icon>close</v-icon>
@@ -48,56 +56,47 @@
                        </div>
                      </v-list-tile>
                  </v-list>
-               </v-card>
-             </v-dialog>
-           </div>
+                </v-card>
+                </v-dialog>
+              </div>
 
-           <div class="airQuality leftContentLayout">
-             <div>空气质量：<span>良</span></div>
-             <div>PM2.5：<span>25</span></div>
-           </div>
+              <div class="airQuality">
+                <div>空气质量：<span>良</span></div>
+                <div>PM2.5：<span>25</span></div>
+              </div>
 
-           <div class="leftContentLayout">
-             <div class="temperature">
-               <a><span>sd</span>℃</a>
-               <p>体感温度:  ℃</p>
-             </div>
-
-             <div class="temperature-right">
-               <div></div>
+              <div class="temperature">
+                <a><span>56</span>℃</a>
                 <img src="~/assets/inco-weather/100.png"/>
-                <span>晴天</span>
-             </div>
-           </div>
+                <a class="weatherIcon">晴天</a>
+              </div>
+      
+              <div class="firstRow">
+                <div class="humidity">
+                  <div>湿度：40</div>
+                  <div class="PrecipitationRate">降水率：0</div>
+              </div>
+        
+              <div class="windDirection">
+                <div>风向：东南</div>
+                <div class="windSpeed">风速：千米每小时</div>
+                </div>
+              </div>
 
-           <div class="firstRow leftContentLayout">
-             <div class="humidity">湿度：40</div>
-             <div class="PrecipitationRate">降水率：0</div>
-           </div>
+              <div class="secondRow leftContentLayout">
+              </div>
+            </div>
 
-           <div class="secondRow leftContentLayout">
-             <div class="wind">
-               <div class="lanren">
-	              <ul>
-	       	        <li><img src="~/static/images/main5-pic1.png"> </li>
-	       	        <li><img src="~/static/images/main5-pic2.png"> </li>
-	       	        <li><img src="~/static/images/main5-pic3.png"> </li>
-	              </ul>
-               </div>
-             </div>
-             <div class="windDirection">风向</div>
-             <div class="windSpeed">风速</div>
-           </div>
-
-          </div>
-
-          <div>
-            kong
-          </div>
-      </div>
-  
-    </div>
-  </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+       <v-flex xs6>
+        <v-card dark color="secondary">
+          <v-card-text class="px-0">撒旦</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -107,8 +106,52 @@ export default {
       dialog: false,
       notifications: false,
       sound: true,
-      widgets: false
+      widgets: false,
 
+      headers: [
+        {
+          text: '日期',
+          sortable: false
+        },
+        { text: '天气',
+          sortable: false },
+        { text: '气温',
+          sortable: false },
+        { text: '风向',
+          sortable: false },
+        { text: '相对湿度',
+          sortable: false },
+        { text: '能见度',
+          sortable: false }
+      ],
+      desserts: [
+        {
+          value: false,
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%'
+        },
+        {
+          value: false,
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7% '
+        }
+      ]
     }
   }
 }
@@ -116,105 +159,49 @@ export default {
 
 
 <style scoped lang="less">
-.container {
-  padding-top: 10px;
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-}
-.container-content {
-  width: 80%;
-  background-color: #666;
-}
-
-.lanren {
-  -webkit-transition: All 1s 0s;
-  -moz-transition: All 1s 0s;
-  -ms-transition: All 1s 0s;
-  margin-top: 20px;
-  ul{ 
-    margin: 0;
-    padding: 0;
-    -webkit-transform: translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
-    -moz-transform: translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
-    -ms-transform: translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
-    li {
-      list-style: none;
-      position: absolute;
-      width: 80px;
-      height: 80px;
-      img {
-        width: 80px;
-        height: 80px;
-        -webkit-transform-style: preserve-3d;
-        -moz-transform-style: preserve-3d;
-        -ms-transform-style: preserve-3d;
-        transform-style: preserve-3d;
+  .content-right-btn {
+    text-align: initial;
+    padding-top: 15px;
+    margin-left: 15%;
+  }
+  .content-right-top-left {
+    width: 50%;
+    .airQuality{
+      display: flex;
+      flex-flow: row wrap;
+      justify-content: flex-end;
+    }
+    .temperature {
+      margin-left: 25%;
+      .weatherIcon {
+        font-size: 18px;
       }
     }
-  }
-  }
-
-  .lanren ul li:nth-of-type(1) {
-    -webkit-transition: All 1.2s 0s;
-    -moz-transition: All 1.2s 0s;
-    -ms-transition: All 1.2s 0s;
-    transition: All 1.2s 0s;
-    -webkit-transform: translateX(0) translateY(0) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
-    -moz-transform: translateX(0) translateY(0) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
-    -ms-transform: translateX(0) translateY(0) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
-    transform: translateX(0) translateY(0) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1);
-    img {
-      -webkit-animation: z 5s linear 0s infinite;
-      -moz-animation: z 5s linear 0s infinite;
-      -ms-animation: z 5s linear 0s infinite;
-      animation: z 5s linear 0s infinite;
+    .temperature-right {
+      margin-right: auto;
+      div {
+        height: 55%;
+      }
     }
-  }
-  .lanren ul li:nth-of-type(2) {
-    -webkit-animation: y 20s linear 0s infinite;
-    -moz-animation: y 20s linear 0s infinite;
-    -ms-animation: y 20s linear 0s infinite;
-    animation: y 20s linear 0s infinite;
-  }
-  .lanren ul li:nth-of-type(3) {
-    -webkit-animation: z 10s linear 0s infinite;
-    -moz-animation: z 10s linear 0s infinite;
-    -ms-animation: z 10s linear 0s infinite;
-    animation: z 10s linear 0s infinite;
-  }
-  @-webkit-keyframes z {
-	  from { -webkit-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -moz-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -ms-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); }
-	  to { -webkit-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(360deg) scaleX(1) scaleY(1) scaleZ(1); -moz-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(360deg) scaleX(1) scaleY(1) scaleZ(1); -ms-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(360deg) scaleX(1) scaleY(1) scaleZ(1); transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(360deg) scaleX(1) scaleY(1) scaleZ(1); }
-  }
-  @-webkit-keyframes y {
-	  from { -webkit-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -moz-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); -ms-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scaleX(1) scaleY(1) scaleZ(1); }
-	  to { -webkit-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(-360deg) scaleX(1) scaleY(1) scaleZ(1); -moz-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(-360deg) scaleX(1) scaleY(1) scaleZ(1); -ms-transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(-360deg) scaleX(1) scaleY(1) scaleZ(1); transform: translateX(0) translateY(0) translateZ(0) rotateX(0deg) rotateY(0deg) rotateZ(-360deg) scaleX(1) scaleY(1) scaleZ(1); }
-  }
-  .content-right-top {
-    padding: 20px 20px 5px;
-    display: flex;
-    flex-flow: row;
-    justify-content: space-between;
-    align-items: center;
-    .content-right-top-left {
-      width: 50%;
-      .leftContentLayout {
+    .firstRow {
+      margin-left: 25%;
+      display: flex;
+      justify-content: space-between;
+      .humidity {
         display: flex;
-        flex-flow: row wrap;
-        justify-content: space-around;
-        margin: 15px;
-        .temperature {
-          margin-left: 50px;
-        }
-        .temperature-right {
-          margin-right: auto;
-          div {
-            height: 55%;
-          }
-
+      }
+      .PrecipitationRate {
+        margin-left: 15px;
+      }
+      .windDirection {
+        display: flex;
+        margin-right: 205px;
+        .windSpeed {
+          margin-left: 20px;
         }
       }
+    }
+  }
       .temperature {
         a {
           font-size: 3em;
@@ -223,11 +210,20 @@ export default {
           font-size: 4em;
         }
       }
+
+    .content-right-bottom {
+      width: 80%;
+      margin-left: 10%;
+      margin-top: 15px;
+      display: flex;
+      flex-flow: row wrap;
+      justify-content: space-around;
     }
     .content-address {
       display: flex;
       flex-flow: row;
       align-items: center;
+      margin-left: 25%;
       span {
         font-size: 25px;
         margin-right: 5px;
@@ -239,6 +235,10 @@ export default {
         width: 38px;
       }
     }
-  } 
+    .right-form {
+      margin-right: 10%;
+      margin-left: 10%;
+      width: 50%;
+    }
 
 </style>
